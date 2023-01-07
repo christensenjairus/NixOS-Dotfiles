81d53c349f2aa0c730872b756ad1fa58b59e9158
Jairus Christensen <christensenjairus@gmail.com>
2023-01-07 11:24:14 -0700

    Power management finally works as intended

--- .config/awesome/power-management.lua
+++ .config/awesome/power-management.lua
@@ -1,7 +1,8 @@
 local awful = require("awful") --Everything related to window managment
 
---Enable Energy Star Power Management (blank screen after some time, but not controlling lock or suspend)
-awful.spawn.with_shell("xset +dpms")
+--Disable Energy Star Power Management (blank screen after some time, but not controlling lock or suspend)
+--May mess with lock mechanism
+awful.spawn.with_shell("xset -dpms")
 
 --1st number is seconds from idle until lock
 --2nd number is seconds from lock until suspend
@@ -9,4 +10,4 @@ awful.spawn.with_shell("xset s 300 900")
 --Lock after 300s (5m), suspend after another 900s (15m)
 
 --Lock screen & suspend commands and program to execute them
-awful.spawn.with_shell("killall xss-lock; xss-lock -n 'i3lock-fancy-rapid 1 50' -- systemctl suspend-then-hibernate &")
+awful.spawn.with_shell("killall xss-lock; xss-lock -n 'i3lock-fancy-rapid 5 10' -- systemctl suspend-then-hibernate &")
